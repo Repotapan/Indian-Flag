9e12f2edc75a62794052cef414a9a06941915637
Tapan <tpati2001@gmail.com>
2023-01-30 12:00:54 +0530

    first commit

---
+++ fibonancci.py
@@ -0,0 +1,15 @@
+def fiboc(n):
+    if n<0:
+        print("invalid input")
+    elif n==0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+
+          
+    else:
+        return fiboc(n-1) + fiboc(n-2 )
+
+        
+print(fiboc(6))
+
---
+++ text.py
@@ -0,0 +1,4 @@
+from p.module1 import f1
+from p.q.module2 import f2
+f1()
+f2()
